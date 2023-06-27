b8f915960f4b8110afae0c6cb4276fc657fd9f28
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-27 16:10:09 +0200

    missing component?

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+const input = "https://flynn.boolean.careers/exercises/api/random/mail";
 console.log("Vue ok", Vue);
+const { createApp } = Vue;
+const app = createApp({
+  data() {
+    return {
+      emails: [],
+    };
+  },
+  methods: {
+    getEmails() {
+      for (i = 0; i < 10; i++) {
+        axios.get(input).then((res) => {
+          console.log(res);
+        });
+        // this.emails.push(email);
+      }
+    },
+  },
+});
+app.mount("#root");
